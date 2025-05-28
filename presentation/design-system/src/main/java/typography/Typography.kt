7e21b99f7e13bdd96a6fc3552e94fdb5d6a3922f
woong <workplayhard1@naver.com>
2025-05-28 17:37:20 +0900

    feat: 폰트 추가

---
+++ presentation/design-system/src/main/java/typography/Typography.kt
@@ -0,0 +1,69 @@
+package com.example.presentation.designsystem.typography
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import typography.Pretendard
+
+object Typography {
+    val h1 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 22.sp * 1.3f
+    )
+    val h2 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 20.sp * 1.3f
+    )
+    val h3 = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 18.sp * 1.3f
+    )
+    var body1Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 16.sp * 1.3f
+    )
+    var body1Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 16.sp * 1.5f
+    )
+    var body2Bold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 14.sp * 1.3f
+    )
+    val body2Regular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 14.sp * 1.5f
+    )
+    var captionBold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        lineHeight = 12.sp * 1.3f
+    )
+    var captionSemiBold = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 12.sp * 1.5f
+    )
+    var captionRegular = TextStyle(
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 12.sp * 1.5f
+    )
+}
