1ddeb756243cea47c145044bbed2a5934dff0522
woong <workplayhard1@naver.com>
2025-07-31 17:49:40 +0900

    feat: 비밀댓글은 비밀답글만을 달수있도록 수정구현

--- presentation/feature/common/src/main/java/comment_bottom_sheet/CommentBottomSheet.kt
+++ presentation/feature/common/src/main/java/comment_bottom_sheet/CommentBottomSheet.kt
@@ -468,7 +468,7 @@ private fun CommentInputBar(
         decorationBox = commentDecorationBox(
             inputState = inputState,
             isFocused = isFocused,
-            onLockClick = onLockClick,
+            onLockClick = { if (inputState.replyToComment?.secret == false) onLockClick() },
             onSendClick = onSendClick,
             onCancelReplyClick = onCancelReplyClick
         ),
